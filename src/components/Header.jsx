4da37278f256ed30388a559c6216ca71ef154b28
Dmytro Slipchenko <dslipchenko@gmail.com>
2023-12-22 01:11:14 +0200

    feat: add header adn hero section

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<div className='wrapper'>
+				<div className='navigation'>
+					<nav className='navigation__links'>
+						<a href='#'>Header</a>
+						<a href='#'>Hero</a>
+						<a href='#'>Summary</a>
+						<a href='#'>Skills</a>
+						<a href='#'>Experience</a>
+						<a href='#'>Education</a>
+						<a href='#'>Portfolio</a>
+					</nav>
+
+					<div className='navigation__social'>
+						<a href='#'>email</a>
+						<a href='#'>git</a>
+						<a href='#'>linkedIn</a>
+						<a href='#'>telegram</a>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+	return (
+		<section className='hero'>
+			<div className='wrapper'>
+				<h1>Dmytro Slipchenko</h1>
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
