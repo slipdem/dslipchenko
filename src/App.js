cc19f0eea97164e5bd6c096879322572c7c09f7f
Dmytro Slipchenko <dslipchenko@gmail.com>
2023-12-30 22:47:42 +0200

    feat: hero, styles, layout

--- src/App.js
+++ src/App.js
@@ -3,10 +3,36 @@ import Hero from './components/Hero';
 
 function App() {
 	return (
-		<>
+		<div className='container'>
 			<Header />
-			<Hero />
-		</>
+			<main className='main'>
+				<aside className='aside'>
+					<ul>
+						<li>
+							<a href='#'>Summary</a>
+						</li>
+						<li>
+							<a href='#'>Skills</a>
+						</li>
+						<li>
+							<a href='#'>Experience</a>
+						</li>
+						<li>
+							<a href='#'>Education</a>
+						</li>
+						<li>
+							<a href='#'>Courses and trainings</a>
+						</li>
+						<li>
+							<a href='#'>Portfolio</a>
+						</li>
+					</ul>
+				</aside>
+				<section className='info'>
+					<Hero />
+				</section>
+			</main>
+		</div>
 	);
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,39 @@ import React from 'react';
 const Hero = () => {
 	return (
 		<section className='hero'>
-			<div className='wrapper'>
-				<h1>Dmytro Slipchenko</h1>
+			<svg
+				role='img'
+				aria-hidden='true'
+				className='svg__dots'>
+				<title>Dot Regular</title>
+				<pattern
+					id='DotRegular-pattern'
+					x='0'
+					y='0'
+					width='8'
+					height='8'
+					patternUnits='userSpaceOnUse'>
+					<circle
+						cx='0.5'
+						cy='0.5'
+						r='0.5'
+						fill='currentColor'></circle>
+				</pattern>
+				<rect
+					width='100%'
+					height='100%'
+					fill='url(#DotRegular-pattern)'></rect>
+			</svg>
+			<div className='hero__info'>
+				<h3 className='hero__info-subtitle'>
+					Frontend Developer / Kyiv, Ukraine
+				</h3>
+				<h1 className='hero__info-title'>Dmytro Slipchenko</h1>
+				<p className='hero__info-text'>
+					Frontend Developer with experience in building and maintaining web
+					applications. Proficient in HTML, CSS, JavaScript, and React. Looking
+					for a position that would allow me to grow as a developer.
+				</p>
 			</div>
 		</section>
 	);
