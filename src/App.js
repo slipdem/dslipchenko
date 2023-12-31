9b9a3d259593e8148eb8764fa3489efdcc4d9f71
Dmytro Slipchenko <dslipchenko@gmail.com>
2023-12-31 16:46:32 +0200

    feat: add new boxes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Box from './components/Box';
 import Header from './components/Header';
 import Hero from './components/Hero';
 
-
 function App() {
 	return (
 		<div className='container'>
@@ -12,9 +11,63 @@ function App() {
 				<Aside />
 				<section className='info'>
 					<Hero />
-					<Box title='Skills' className='skills'>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus harum ipsum inventore natus dolorem voluptatum culpa dignissimos, labore corrupti autem officiis doloribus quasi nam accusamus, alias hic voluptate debitis accusantium!</p>
-					</Box>
+					<div className='wrapper'>
+						<Box
+							title='Skills'
+							className='skills'>
+							<ul className='box__list'>
+								<li className='box__list-item'>
+									<span>Web:</span>HTML/HTML5, CSS, SCSS/SASS
+								</li>
+								<li className='box__list-item'>
+									<span>Programming Languages:</span>JavaScript, Typescript
+									(basic), React
+								</li>
+								<li className='box__list-item'>
+									<span>VCS:</span>Git
+								</li>
+								<li className='box__list-item'>
+									<span>Languages:</span>English (Intermediate)
+								</li>
+								<li className='box__list-item'>
+									<span>Other:</span>Redux, Figma, Adobe Photoshop, Bootstrap,
+									Adaptive layout, BEM
+								</li>
+							</ul>
+						</Box>
+					</div>
+					<div className='wrapper'>
+						<Box
+							title='Experience'
+							className='experience'>
+							<h2 className='experience__title'>MediaHero</h2>
+							<p className='experience__position'>
+								<span>position:</span>
+								web developer (full time remote)
+							</p>
+							<p className='experience__date'>01/2017 - 11/2022</p>
+							<p className='experience__info'>
+								Build websites, maintain web-servers and hosting
+							</p>
+							<ul className='box__list'>
+								<li className='box__list-item'>
+									using HTML, CSS, and JavaScript, to create and maintain
+									websites and landing pages
+								</li>
+								<li className='box__list-item'>
+									build and maintain websites on WordPress
+								</li>
+								<li className='box__list-item'>
+									managed the day-to-day operations of Google Ads accounts,
+									including account setup and create and launch new campaigns
+								</li>
+								<li className='box__list-item'>
+									maintain optimal website performance and manage hosting
+									environment
+								</li>
+							</ul>
+						</Box>
+					</div>
 				</section>
 			</main>
 		</div>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const Box = ({ title, className, children }) => {
+const Box = ({ title, className, children, position, date }) => {
 	return (
 		<article className={`box ${className}`}>
 			<header className='box__header'>{title}</header>
-			{children}
+			<div className='box__children'>{children}</div>
+			{
+				// if pos & date === undefined => return empty skills footer
+				// if pos & date !== undefined => return footer with experience
+				// if location !== undefined => return footer with education
+			}
+			<footer className='box__footer'></footer>
 		</article>
 	);
 };
