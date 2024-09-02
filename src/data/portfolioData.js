783f57015d534f196c223170dacdb633bfffcf86
Dmytro Slipchenko <dslipchenko@gmail.com>
2024-09-02 14:31:20 +0300

    chore: add portfolio descriptions, favicon

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -3,7 +3,8 @@ export const portfolioData = [
 		cardTitle: 'Portfolio',
 		projectTitle: 'Furniture LP',
 		spec: ['HTML', 'SASS/SCSS', 'JavaScript', 'GSAP', 'swiper', 'GIT'],
-		desription: 'Some smart text Furniture LP',
+		desription:
+			'A minimalist landing page for furniture, crafted with HTML and styled using SASS. It features smooth, engaging animations powered by GSAP, providing an elegant user experience. Perfect for showcasing modern furniture designs with a clean and responsive layout, ensuring a seamless interaction across all devices.',
 		link: 'https://furni-lp.netlify.app/',
 		gitLink: 'https://github.com/slipdem/furniture-lp-parcel',
 		img: './img/portfolio_previews/furni_lp_preview.webp',
@@ -12,7 +13,8 @@ export const portfolioData = [
 		cardTitle: 'Portfolio',
 		projectTitle: 'BOSE Audio LP',
 		spec: ['HTML', 'CSS', 'JavaScript', 'GIT'],
-		desription: 'Some smart text BOSE Audio LP',
+		desription:
+			"A sleek and minimalist landing page for BOSE Audio, designed with responsive HTML, CSS, and JavaScript. This clean layout focuses on delivering a refined user experience, highlighting BOSE's premium audio products with simplicity and elegance, ensuring seamless functionality across all devices.",
 		link: 'https://bose-lp.netlify.app/',
 		gitLink: 'https://github.com/slipdem/bose-landing',
 		img: './img/portfolio_previews/bose_lp_preview.webp',
@@ -21,14 +23,15 @@ export const portfolioData = [
 		cardTitle: 'Portfolio',
 		projectTitle: 'Simply Invest LP',
 		spec: ['HTML', 'CSS', 'JavaScript', 'GIT'],
-		desription: 'Some smart text about Simply Invest LP',
+		desription:
+			'A modern and straightforward landing page for Simply Invest, introducing a new investment platform that offers income-generating opportunities. Built with responsive HTML, CSS, and JavaScript, this LP provides a clean and user-friendly interface, ensuring an effortless experience for potential investors across all devices.',
 		link: 'https://simply-invest.netlify.app/',
 		gitLink: 'https://github.com/slipdem/invest-land',
 		img: './img/portfolio_previews/simply_invest_lp_preview.webp',
 	},
 	{
 		cardTitle: 'Portfolio',
-		projectTitle: 'Ciklum Coursework',
+		projectTitle: 'Ciklum course project / undergoing rework',
 		spec: [
 			'React',
 			'SASS/SCSS',
@@ -38,14 +41,15 @@ export const portfolioData = [
 			'MUI',
 			'GIT',
 		],
-		desription: 'book store (rework ongoing)',
+		desription:
+			'A course project from Ciklum: a simple book store with a minimalist design. The platform offers easy user registration, seamless book additions to the cart, and detailed book descriptions. Built using React, SASS/SCSS, React Context API, and REST API, it delivers a clean, efficient experience for browsing and purchasing books.',
 		link: 'https://slipdem.github.io/x-course-task/',
 		gitLink: 'https://github.com/slipdem/x-course-task',
 		img: './img/portfolio_previews/ciklum_bookstore_preview.webp',
 	},
 	{
 		cardTitle: 'Portfolio',
-		projectTitle: 'teamchallenge.io',
+		projectTitle: 'teamchallenge.io / ongoing',
 		spec: [
 			'React',
 			'SASS/SCSS',
@@ -57,7 +61,8 @@ export const portfolioData = [
 			'JIRA',
 		],
 		desription:
-			'teamwork, training project (ongoing), whole team link https://teamchallenge.io/team/449#team',
+			'A pet project from teamchallenge.io: a full-fledged furniture store featuring various categories, a catalog, a cart, search functionality, and an order form. Developed as a team effort following Scrum methodology, this project leverages React, SASS, Redux, Redux Toolkit, REST API, and JIRA for efficient project management and collaboration.',
+		// 'teamwork, training project (ongoing), whole team link https://teamchallenge.io/team/449#team',
 		link: 'https://urbanhome.netlify.app/',
 		gitLink: 'https://github.com/beznavorotov/furniture',
 		img: './img/portfolio_previews/urbanhome_preview.webp',
